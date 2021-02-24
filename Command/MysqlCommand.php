3dac81b87831f6316b0bc2e64c7603e079fef552
Vincent Chalnot <vchalnot@clever-age.com>
2021-02-24 06:01:17 +0100

    Fixing compatibility with Sf4+

--- Command/MysqlCommand.php
+++ Command/MysqlCommand.php
@@ -1,19 +1,36 @@
 <?php
+/*
+ * This file is part of the Sidus/DatabaseMaintenanceBundle package.
+ *
+ * Copyright (c) 2021 Vincent Chalnot
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace Sidus\DatabaseMaintenanceBundle\Command;
 
 use Doctrine\DBAL\Connection;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class MysqlCommand extends ContainerAwareCommand
+/**
+ * Query mysql with passthru command
+ */
+class MysqlCommand extends Command
 {
-    /**
-     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
-     */
-    protected function configure()
+    public function __construct(
+        protected ManagerRegistry $doctrine,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
     {
         $this
             ->setName('sidus:database:mysql')
@@ -21,19 +38,11 @@ class MysqlCommand extends ContainerAwareCommand
             ->setDescription('Command alias to mysql client with the proper parameters');
     }
 
-    /**
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @throws \Exception
-     *
-     * @return null|int null or 0 if everything went fine, or an error code
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         /** @var Connection $connection */
-        $connection = $this->getContainer()->get('doctrine')->getConnection($input->getOption('connection'));
-        if ($connection->getDatabasePlatform()->getName() !== 'mysql') {
+        $connection = $this->doctrine->getConnection($input->getOption('connection'));
+        if ('mysql' !== $connection->getDatabasePlatform()->getName()) {
             throw new \LogicException('Only MySQL database is supported');
         }
 
